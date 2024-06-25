586e57e43262697cb6d597d1fd9a98e9351d7294
Romain Billon <romain.billon@agicap.com>
2024-06-25 21:42:02 +0200

    chore: enhance comment
    Signed-off-by: Romain Billon <romain.billon@agicap.com>

--- src/main.sh
+++ src/main.sh
@@ -246,7 +246,7 @@ function main {
 
   if [[ "${tg_comment}" == "1" ]]; then
     comment "<details>
-<summary>## 🚀 Summary - Execution of \"$tg_command\" in \"${tg_dir}\" ${ICON} </summary>
+<summary>${ICON} Summary - Execution of \"$tg_command\" in \"${tg_dir}\" </summary>
 
 \`\`\`terraform
 ${terragrunt_output}
